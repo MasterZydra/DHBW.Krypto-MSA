07e94e9922274df29f9d89b532c1d833b065a96b
David Hein <david-hein196@web.de>
2020-05-18 17:50:05 +0200

    Add class to load component

---
+++ src/main/CryptoLoader.java
@@ -0,0 +1,37 @@
+package main;
+
+import main.configuration.Configuration;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class CryptoLoader {
+    private Object port;
+    private Method cryptoMethod;
+
+    public void createCryptographyMethod(CryptoMethod method) {
+        Object instance;
+        try {
+            URL[] urls = {new File(Configuration.instance.getCryptoAlgorithmPath()).toURI().toURL()};
+            URLClassLoader urlClassLoader = new URLClassLoader(urls, CryptoLoader.class.getClassLoader());
+            Class clazz = Class.forName(Configuration.instance.cryptoAlgorithm.toString(), true, urlClassLoader);
+
+            instance = clazz.getMethod("getInstance").invoke(null);
+            port = clazz.getDeclaredField("port").get(instance);
+            cryptoMethod = port.getClass().getMethod(method.toString().toLowerCase(), String.class, File.class);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Object getPort() {
+        return port;
+    }
+
+    public Method getCryptoMethod() {
+        return cryptoMethod;
+    }
+}
