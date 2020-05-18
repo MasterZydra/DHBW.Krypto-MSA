9a217399e19c47ebf46aa5c2a34fec442eb87c53
David Hein <david-hein196@web.de>
2020-05-18 17:49:40 +0200

    Extend config with function to get path to crypto jar

--- src/main/configuration/Configuration.java
+++ src/main/configuration/Configuration.java
@@ -1,19 +1,40 @@
 package main.configuration;
 
+import main.CryptoAlgorithm;
+
 public enum Configuration {
     instance;
 
+    public CryptoAlgorithm cryptoAlgorithm = CryptoAlgorithm.RSA;
+
     // common
-    public final String userDirectory = System.getProperty("user.dir");
-    public final String fileSeparator = System.getProperty("file.separator");
+    public final String ud = System.getProperty("user.dir");
+    public final String fs = System.getProperty("file.separator");
 
     // database
-    public final String dataDirectory = userDirectory + fileSeparator + "hsqldb" + fileSeparator;
+    public final String dataDirectory = ud + fs + "hsqldb" + fs;
     public final String databaseFile = dataDirectory + "datastore.db";
     public final String driverName = "jdbc:hsqldb:";
     public final String username = "sa";
     public final String password = "";
 
     // component
-    public String componentDirectory = userDirectory + fileSeparator + "component";
+    public String componentDirectory = ud + fs + "component";
+
+    public String getCryptoAlgorithmPath() {
+        String path = componentDirectory;
+        switch (cryptoAlgorithm) {
+            case RSA:
+                path += fs + cryptoAlgorithm.toString().toLowerCase() + fs + "jar" + fs + "RSA.jar";
+                break;
+            case Shift:
+                path += fs + cryptoAlgorithm.toString().toLowerCase() + fs + "jar" + fs + "Shift.jar";
+                break;
+            default:
+                path = "ERROR";
+        }
+        return path;
+    }
+
+    public String getKeyFilePath = ud + fs + "keyfiles" + fs;
 }
