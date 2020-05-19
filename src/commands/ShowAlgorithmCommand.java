99b2f2016e890cc7682b7489bb21c25294094b9f
mrem_INF18A <michael.rempel@gmx.net>
2020-05-19 12:14:08 +0200

    implement ShowAlgorithmCommand.execute() via Storage/GuiController

--- src/commands/ShowAlgorithmCommand.java
+++ src/commands/ShowAlgorithmCommand.java
@@ -1,5 +1,9 @@
 package commands;
 
+import configuration.Configuration;
+
+import java.util.List;
+
 public class ShowAlgorithmCommand implements ICommand {
     public void setParam(String name, String value) {
 
@@ -10,6 +14,14 @@ public class ShowAlgorithmCommand implements ICommand {
     }
 
     public void execute() {
+        List<String> list = Configuration.instance.getAlgorithmFileNames();
+        String output= "";
+        for (String algo: list
+             ) {
+            if(!output.isBlank()) output+=" | ";
+            output += algo;
 
+        }
+        Configuration.storage.guiController.displayText(output);
     }
 }
