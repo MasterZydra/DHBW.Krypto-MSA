45939a3810e8d09dd0b77a3d13662d9a37cdc644
mrem_INF18A <michael.rempel@gmx.net>
2020-05-19 18:03:35 +0200

    add getLatestLog() to Logger

--- src/logger/LoggerMSA.java
+++ src/logger/LoggerMSA.java
@@ -7,7 +7,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class LoggerMSA {
 
@@ -15,9 +21,10 @@ public class LoggerMSA {
     File  logFile = null;
 
     public LoggerMSA(String cryptographyActionType, String algorithm) {
-        String outputFileString = logDirectory + cryptographyActionType + "_"
+        String fileName = cryptographyActionType + "_"
                 + algorithm + "_"
                 + Instant.now().getEpochSecond() + ".txt";
+        String outputFileString = logDirectory + Configuration.instance.fs + fileName;
         logFile = new File(outputFileString);
         new File(logDirectory).mkdir();
     }
@@ -34,8 +41,25 @@ public class LoggerMSA {
         }
     }
 
+    static public String getLatestLog() {
+        List<String> filedates = new ArrayList<>();
+        String path = Configuration.instance.logDirectory;
+        Long youngestTimeStamp = 0L;
+        Path newestLogFile=null;
+        try {
+            newestLogFile = Files.walk(Paths.get(path))
+                    .filter(Files::isRegularFile)
+                    .max(Comparator.naturalOrder()).orElse(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(newestLogFile==null?null:newestLogFile.getFileName().toString());
+        return newestLogFile==null?null:newestLogFile.getFileName().toString();
+     }
+
     public static void main(String[] args) {
         LoggerMSA lt = new LoggerMSA("encode", "none");
         lt.log("message!");
+        System.out.println("latest: "+LoggerMSA.getLatestLog());
     }
 }
