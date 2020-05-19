bf9e3554ae2a12aaeca21df117279eac74590142
mrem_INF18A <michael.rempel@gmx.net>
2020-05-19 20:01:39 +0200

    fix log string indexing

--- src/logger/LoggerMSA.java
+++ src/logger/LoggerMSA.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -44,9 +46,9 @@ public class LoggerMSA {
         Path newestLogFile=null;
         Comparator<Path> byTimeStamp = (path1, path2) -> {
             String path1str = path1.getFileName().toString();
-            long timeStamp1 = Long.parseLong( path1str.substring(path1str.lastIndexOf("_"), path1str.length() - 4) );
+            long timeStamp1 = Long.parseLong( path1str.substring(path1str.lastIndexOf("_")+1, path1str.length() - 4) );
             String path2str = path2.getFileName().toString();
-            long timeStamp2 = Long.parseLong( path2str.substring(path2str.lastIndexOf("_"), path2str.length() - 4) );
+            long timeStamp2 = Long.parseLong( path2str.substring(path2str.lastIndexOf("_")+1, path2str.length() - 4) );
             return Long.compare(timeStamp1,timeStamp2);
         };
 
@@ -62,13 +64,21 @@ public class LoggerMSA {
         try (BufferedReader br = new BufferedReader(new FileReader(newestLogFile.toFile(), StandardCharsets.UTF_8)))
         {
             String line=null;
-
+            String fileName = newestLogFile.getFileName().toString();
+            sb.append("Log:\n").append(fileName).append("\n");
+            Instant time = Instant.ofEpochSecond(Long.parseLong( fileName.substring(fileName.lastIndexOf("_")+1, fileName.length() - 4) ));
+            LocalDateTime ldt = LocalDateTime.ofInstant(time, ZoneId.systemDefault());
+            String date = String.format("%d %s %d at %d:%d%n", ldt.getDayOfMonth(), ldt.getMonth(),
+                    ldt.getYear(), ldt.getHour(), ldt.getMinute());
+            sb.append(date + "\n");
             while ((line = br.readLine()) != null) {
-                sb.append(line);
+                sb.append(line).append("\n");
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
         //return newestLogFile==null?null:newestLogFile.getFileName().toString();
              return sb.toString();
