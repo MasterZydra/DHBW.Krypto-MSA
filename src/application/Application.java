e8c9c11a47873f124fe10e6a9d47907c31faba57
mrem_INF18A <michael.rempel@gmx.net>
2020-05-20 15:28:45 +0200

    implement loading channels from DB

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,17 +1,22 @@
 package application;
 
 
+import configuration.RuntimeStorage;
 import cryptography.CryptoAlgorithm;
 import gui.GUI;
 import persistence.IMsaDB;
 import persistence.MSA_HSQLDB;
+import persistence.dataModels.Channel;
+
+import java.util.List;
 
 public class Application {
     public static void main(String... args) {
+        IMsaDB db = MSA_HSQLDB.instance;
         Application a = new Application();
         a.init();
         a.startGui();
-
+        RuntimeStorage.instance.guiController.displayText(db.getAlgorithms().toString());
     }
 
     private void startGui() {
@@ -25,10 +30,20 @@ public class Application {
     private void loadNetworksFromDatabase() {
         IMsaDB db = MSA_HSQLDB.instance;
         db.setupConnection();
-        //db.createAllTables();
+        List<Channel> channels = db.getChannels();
+        db.dropAllTables();
+        db.createAllTables();
         for (CryptoAlgorithm algo: CryptoAlgorithm.values()
         ) {
             db.insertAlgorithm(algo.toString());
+        }
+        for (Channel channel : channels
+             ) {
+            db.insertType(channel.getParticipantA().getType());
+            db.insertType(channel.getParticipantB().getType());
+            db.insertParticipant(channel.getParticipantA());
+            db.insertParticipant(channel.getParticipantB());
+            db.insertChannel(channel);
         }
         db.shutdown();
     }
