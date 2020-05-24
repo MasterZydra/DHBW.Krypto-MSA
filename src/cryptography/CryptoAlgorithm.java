33f7decf1f47c5101b314c8d4c89d16424a2de39
mrem_INF18A <michael.rempel@gmx.net>
2020-05-24 14:45:02 +0200

    add CryptoAlgorithm.valueOfCaseIgnore()

--- src/cryptography/CryptoAlgorithm.java
+++ src/cryptography/CryptoAlgorithm.java
@@ -3,4 +3,16 @@ package cryptography;
 public enum CryptoAlgorithm {
     Shift,
     RSA;
+
+    public static CryptoAlgorithm valueOfCaseIgnore(String algoName) {
+
+        for (CryptoAlgorithm algo : CryptoAlgorithm.values()
+                ) {
+            if (algo.toString().equalsIgnoreCase(algoName)) {
+                return algo;
+            }
+        }
+        return null;
+    }
 }
+
