8452920d85bbb72672455b32603481462f97afd0
David Hein <david-hein196@web.de>
2020-05-30 14:03:00 +0200

    Implement logic for en- and decryption command

--- src/commands/DecryptMessageCommand.java
+++ src/commands/DecryptMessageCommand.java
@@ -1,9 +1,44 @@
 package commands;
 
+import configuration.*;
+import cryptography.*;
+import logger.LoggerMSA;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
 public class DecryptMessageCommand extends CqrCommand {
+    private LoggerMSA logger;
 
     @Override
     public void execute() {
+        CryptoLoader loader = new CryptoLoader();
+        try {
+            // Load component
+            loader.createCryptographyMethod(CryptoAlgorithm.valueOfCaseIgnore(getParam("algorithm")), CryptoMethod.DECRYPT);
+            Method method = loader.getCryptoMethod();
 
+            // Build file
+            String fileName = getParam("keyfile");
+            if (!fileName.toLowerCase().endsWith(".json"))
+                fileName += ".json";
+            File file = new File(Configuration.instance.getKeyFilePath + fileName);
+
+            // Decrypt message
+            String decrypted = (String) method.invoke(loader.getPort(), getParam("message"), file);
+            printMessage(decrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            printFailMessage();
+            return;
+        }
+    }
+
+    private void printFailMessage() {
+        printMessage("Could not process DecryptMessage command");
+    }
+
+    private void printMessage(String failMessage) {
+        RuntimeStorage.instance.guiController.displayText(failMessage);
     }
 }
--- src/commands/EncryptMessageCommand.java
+++ src/commands/EncryptMessageCommand.java
@@ -1,10 +1,44 @@
 package commands;
 
+import configuration.*;
+import cryptography.*;
+import logger.LoggerMSA;
+
+import java.io.File;
+import java.lang.reflect.Method;
 
 public class EncryptMessageCommand extends CqrCommand {
+    private LoggerMSA logger;
 
     @Override
     public void execute() {
+        CryptoLoader loader = new CryptoLoader();
+        try {
+            // Load component
+            loader.createCryptographyMethod(CryptoAlgorithm.valueOfCaseIgnore(getParam("algorithm")), CryptoMethod.ENCRYPT);
+            Method method = loader.getCryptoMethod();
 
+            // Build file
+            String fileName = getParam("keyfile");
+            if (!fileName.toLowerCase().endsWith(".json"))
+                fileName += ".json";
+            File file = new File(Configuration.instance.getKeyFilePath + fileName);
+
+            // Encrypt message
+            String encrypted = (String) method.invoke(loader.getPort(), getParam("message"), file);
+            printMessage(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            printFailMessage();
+            return;
+        }
+    }
+
+    private void printFailMessage() {
+        printMessage("Could not process EncryptMessage command");
+    }
+
+    private void printMessage(String failMessage) {
+        RuntimeStorage.instance.guiController.displayText(failMessage);
     }
 }
