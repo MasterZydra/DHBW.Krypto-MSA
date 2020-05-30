3ee977e9d5a7eda5a02e873b0b03dc54dbef49f3
mrem_INF18A <michael.rempel@gmx.net>
2020-05-30 11:46:35 +0200

    add getParticipant() to network

--- src/network/IChannel.java
+++ src/network/IChannel.java
@@ -1,6 +1,9 @@
 package network;
 
+import java.util.List;
+
 public interface IChannel {
     void sendMessage(MessageEvent message);
     void addParticipant(Participant participant);
+    List<Participant> getParticipants();
 }
