774ef321045b5720a9e80c4a16c3eea546d6014c
David Hein <david-hein196@web.de>
2020-05-30 20:12:57 +0200

    First logic implementation

--- src/commands/RegisterParticipantCommand.java
+++ src/commands/RegisterParticipantCommand.java
@@ -1,9 +1,31 @@
 package commands;
 
+import configuration.RuntimeStorage;
+import persistence.IMsaDB;
+import persistence.dataModels.Participant;
+
 public class RegisterParticipantCommand extends CqrCommand {
+    IMsaDB db = RuntimeStorage.instance.db;
 
     @Override
     public void execute() {
+        // Check if participant exists
+        if (db.participantExists(getParam("participant"))) {
+            String returnMsg = "participant %s already exists, using existing postbox_%s";
+            returnMsg = String.format(returnMsg, getParam("participant"), getParam("participant"));
+            RuntimeStorage.instance.guiController.displayText(returnMsg);
+            return;
+        }
 
+        // Add participant to DB
+        Participant participant = new Participant(getParam("participant"), getParam("type"));
+        db.insertParticipant(participant);
+
+        // Add postbox
+        // TODO Add postbox
+
+        String returnMsg = "participant %s with type %s registered and postbox_%s created";
+        returnMsg = String.format(returnMsg, getParam("participant"), getParam("type"), getParam("participant"));
+        RuntimeStorage.instance.guiController.displayText(returnMsg);
     }
 }
