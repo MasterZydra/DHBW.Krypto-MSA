e83bc4ed205b4689394e00faf5c6bc231c9bab2a
David Hein <david-hein196@web.de>
2020-05-31 22:52:36 +0200

    Add task to do the cracking

---
+++ src/cryptography/CrackTask.java
@@ -0,0 +1,38 @@
+package cryptography;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+public class CrackTask implements Callable<String> {
+    private String message;
+    private String algorithm;
+    private File keyfile;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public void setKeyfile(File keyfile) {
+        this.keyfile = keyfile;
+    }
+
+    @Override
+    public String call() throws Exception {
+        CryptoLoader loader = new CryptoLoader();
+        try {
+            // Load component
+            loader.createCrackerMethod(CryptoAlgorithm.valueOfCaseIgnore(algorithm));
+
+            // Decrypt message
+            String cracked = (String) loader.getCryptoMethod().invoke(loader.getPort(), message, keyfile);
+            return cracked;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
